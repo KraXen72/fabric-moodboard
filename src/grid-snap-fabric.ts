2fe162153334ab79efbb9134b3f0b2fea4f27e5a
KraXen72 <dpelech1@gmail.com>
2023-01-30 15:38:24 +0100

    moooooooore stuff

---
+++ src/grid-snap-fabric.ts
@@ -0,0 +1,44 @@
+import { fabric } from "fabric";
+export class GridSnapFabric extends fabric.Canvas {
+
+  gridGranularity = 20;
+
+  constructor(canvas: HTMLCanvasElement) {
+    super(canvas);
+    this.on('object:scaling', this.onFabricObjectScaling.bind(this));
+  }
+
+  private snapGrid(cord: number): number {
+    return Math.round(cord / this.gridGranularity) * this.gridGranularity;
+  }
+
+  private onFabricObjectScaling(e: fabric.IEvent) {
+    const active = this.getActiveObject();
+    const [width, height] = [active.getScaledWidth(), active.getScaledHeight()];
+
+    // X
+    if (['tl', 'ml', 'bl'].indexOf(e.transform.corner) !== -1) {
+      const tl = this.snapGrid(active.left);
+      active.scaleX = (width + active.left - tl) / (active.width + active.strokeWidth);
+      active.left = tl;
+    } else if (['tr', 'mr', 'br'].indexOf(e.transform.corner) !== -1) {
+      const tl = this.snapGrid(active.left + width);
+      active.scaleX = (tl - active.left) / (active.width + active.strokeWidth);
+    }
+
+    // Y
+    if (['tl', 'mt', 'tr'].indexOf(e.transform.corner) !== -1) {
+      const tt = this.snapGrid(active.top);
+      active.scaleY = (height + active.top - tt) / (active.height + active.strokeWidth);
+      active.top = tt;
+    } else if (['bl', 'mb', 'br'].indexOf(e.transform.corner) !== -1) {
+      const tt = this.snapGrid(active.top + height);
+      active.scaleY = (tt - active.top) / (active.height + active.strokeWidth);
+    }
+
+    // Avoid singularities
+    active.scaleX = (active.scaleY >= 0 ? 1 : -1) * Math.max(Math.abs(active.scaleX), 0.001);
+    active.scaleY = (active.scaleY >= 0 ? 1 : -1) * Math.max(Math.abs(active.scaleY), 0.001);
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,28 @@ import './style.css'
 import { debounce } from './utils';
 import { fabric } from "fabric";
 import { IEvent } from 'fabric/fabric-impl';
+import { GridSnapFabric } from './grid-snap-fabric';
 
 // 
 // https://github.com/Mstrdav/infinitegrid
 // https://github.com/naver/egjs-infinitegrid
 
 const GRID_SIZE = 32 //grid size in px
-const DEFAULT_RECT_OPTS = {
+if (GRID_SIZE % 2 !== 0) throw "GRID_SIZE must be an even number"
+const DEFAULT_RECT_OPTS: fabric.IRectOptions = {
 	originX: 'left',
 	originY: 'top',
-	fill: "red",
+	backgroundColor: "#529d8a",
+	fill: "#529d8a",
 	width: 3 * GRID_SIZE,
-	height: 2 * GRID_SIZE
+	height: 2 * GRID_SIZE,
+	lockRotation: true
 }
 
 // create a wrapper around native canvas element (with id="c")
-const canvas = new fabric.Canvas('c');
+// const _canvas = new fabric.Canvas('c');
+const canvas = new GridSnapFabric(document.getElementById("c") as HTMLCanvasElement)
+canvas.gridGranularity = GRID_SIZE
 
 function resizeMainCanvas() {
 	//https://stackoverflow.com/questions/1248081/how-to-get-the-browser-viewport-dimensions#8876069
@@ -37,11 +43,44 @@ canvas.on('object:moving', function(options: IEvent<MouseEvent>) {
 	});
 });
 
+canvas.on('mouse:down', function(this: any, opt: IEvent<MouseEvent>) {
+  const evt = opt.e;
+  if (evt.altKey === true) {
+    this.isDragging = true;
+    this.selection = false;
+    this.lastPosX = evt.clientX;
+    this.lastPosY = evt.clientY;
+  }
+});
+canvas.on('mouse:move', function(this: any, opt: IEvent<MouseEvent>) {
+  if (this.isDragging) {
+    var e = opt.e;
+    var vpt = this.viewportTransform;
+    vpt[4] += e.clientX - this.lastPosX;
+    vpt[5] += e.clientY - this.lastPosY;
+    this.requestRenderAll();
+    this.lastPosX = e.clientX;
+    this.lastPosY = e.clientY;
+  }
+});
+canvas.on('mouse:up', function(this: any) {
+  // on mouse up we want to recalculate new interaction
+  // for all objects, so we call setViewportTransform
+  this.setViewportTransform(this.viewportTransform);
+	console.log(this.viewportTransform)
+  this.isDragging = false;
+  this.selection = true;
+});
+
 // create a rectangle object
 const rect = new fabric.Rect({
 	left: 0,
 	top: 0, ...DEFAULT_RECT_OPTS
 });
+rect.setControlsVisibility({
+	mtr: false
+})
+rect.contro
 
 // "add" rectangle onto canvas
 canvas.add(rect);
